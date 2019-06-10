2b3c8d0b076b6589bb6701f66c01f054e0b9ddd9
YpchenLove <ypchng@qq.com>
2019-06-10 17:53:42 +0800

    feat: 增加热门图书/book/hot接口

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -2,13 +2,17 @@ const Router = require('koa-router')
 const { HttpException } = require('../../../core/http-exception')
 const { PositiveIntegerValidator } = require('../../validators/validator')
 const { Auth } = require('../../../middlewares/auth')
+const { HotBook } = require('@models/hot-book')
 
 const router = new Router({
     prefix: '/v1/book'
 })
 
 router.get('/hot', new Auth().m, async (ctx, next) => {
-
+    const books = await HotBook.getAll()
+    ctx.body = {
+        books
+    }
 })
 
 module.exports = router
--- app/models/hot-book.js
+++ app/models/hot-book.js
@@ -1,8 +1,43 @@
 const { db } = require('../../core/db')
 const { Sequelize, Model, Op } = require('sequelize')
+const { Favor } = require('./favor')
 
 class HotBook extends Model {
+    // 获取热门图书
+    static async getAll() {
+        const books = await HotBook.findAll({
+            order: ['index']
+        })
+        const ids = []
+        books.forEach((book) => {
+            ids.push(book.id)
+        })
+        const favors = await Favor.findAll({
+            where: {
+                art_id: {
+                    [Op.in]: ids
+                },
+                type: 400
+            },
+            group: ['art_id'],
+            attributes: ['art_id', [Sequelize.fn('COUNT', '*'), 'count']]
+        })
+        books.forEach(book => {
+            HotBook._getEachBookStatus(book, favors)
+        })
+        return books
+    }
 
+    static _getEachBookStatus(book, favors) {
+        let count = 0
+        favors.forEach(favor => {
+            if (book.id === favor.art_id) {
+                count = favor.get('count')
+            }
+        })
+        book.setDataValue('count', count)
+        return book
+    }
 }
 
 HotBook.init({
